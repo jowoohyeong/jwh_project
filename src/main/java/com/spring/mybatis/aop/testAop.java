d97bb41bd3a6e259a57922886ceafbc231ab39af
jwh <jwh5096@naver.com>
2024-01-19 16:03:58 +0900

    프로젝트 설정

--- src/main/java/com/spring/mybatis/aop/testAop.java
+++ src/main/java/com/spring/mybatis/aop/testAop.java
@@ -1,8 +1,5 @@
 package com.spring.mybatis.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-
-@Aspect
 public class testAop {
     public void testAopStart(){
         System.out.println("com.spring.mybatis.aop :: testAopStart()");
